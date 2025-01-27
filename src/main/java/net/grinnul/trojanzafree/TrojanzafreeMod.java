95782a25a1515bba63fc03956a30cfc779e3c530
Grinnul the Dev <Grinnul.kitkutower@gmail.com>
2025-01-27 15:16:14 +0100

    Initial commit

---
+++ src/main/java/net/grinnul/trojanzafree/TrojanzafreeMod.java
@@ -0,0 +1,18 @@
+package net.grinnul.trojanzafree;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TrojanzafreeMod implements ModInitializer {
+	public static final String MOD_ID = "trojanzafree";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/grinnul/trojanzafree/TrojanzafreeModClient.java
@@ -0,0 +1,10 @@
+package net.grinnul.trojanzafree;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrojanzafreeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
